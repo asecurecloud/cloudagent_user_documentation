c2203d9e9f8f5c44b1111df4d6825bd74006cee6
Osman <osman@asecure.cloud>
2025-11-20 12:47:56 -0500

    Initial commit of CloudAgent User Docs

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+    logo: <span>CloudAgent Documentation</span>,
+    project: {
+        link: 'https://github.com/your-repo/cloudagent',
+    },
+    docsRepositoryBase: 'https://github.com/your-repo/cloudagent/blob/main/cloudagent_userdocs',
+    footer: {
+        text: 'CloudAgent Documentation',
+    },
+}
